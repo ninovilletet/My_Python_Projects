738e0be6ac88393d7be5da47bdec701997e62037
Nino <ninovilletet@gmail.com>
2023-12-11 17:10:31 +0100

    Adding another program

---
+++ Guess_the_number.py
@@ -0,0 +1,43 @@
+import random
+from time import sleep
+import os
+
+
+def clear():
+    os.system("cls")
+
+
+clear()
+
+def play_game():
+    num = random.randint(1, 10)
+    print ("A number between 1 and 10 has been chosen.")
+    sleep(2)
+    clear()
+    while True:
+        try:
+            guess = int(input("Take a guess : "))
+        except ValueError:
+            print ("Please type a number !")
+            sleep(2)
+            clear()
+        else:
+            if 1 < guess > 10:
+                print("Please choose a number between 1 and 10 !")
+                sleep(2)
+                clear()
+            elif guess < num:
+                print ("The number to guess is higher.")
+                sleep(1.5)
+                clear()
+            elif guess > num:
+                print ("The number to guess is lower.")
+                sleep(1.5)
+                clear()
+            else:
+                print("Congratulations ! You found the right answer !")
+                sleep(5)
+                break
+
+
+play_game()
